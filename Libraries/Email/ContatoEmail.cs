21a9a0c662312160bd2414a5c3e15477b5d9b21e
Daisy Monte Machado <daisymonte@gmail.com>
2019-10-10 19:27:03 +1100

    Finalizado Seção 8

---
+++ Libraries/Email/ContatoEmail.cs
@@ -0,0 +1,41 @@
+﻿using LojaVirtual.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Libraries.Email
+{
+    public class ContatoEmail
+    {
+        public static void EnviarContatoPorEmail(Contato contato)
+        {
+            // SMTP - Para enviar mensagens
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential("daisymonte@gmail.com", "");
+            smtp.EnableSsl = true;
+
+            string corpoMensagem = string.Format("<h2> Contato - Loja Virtual </h2>" +
+                "<b> Nome: {0} </b><br/> " +
+                "<b> Email: {1} </b><br/>" +
+                "<b> Texto: {2} </b><br/>" +
+                "Email enviado automaticamente do site loja virtual. <br/>",
+                contato.Nome,
+                contato.Email,
+                contato.Texto);
+            // MailMessage - Para construir mensagens
+            MailMessage mensagem = new MailMessage();
+            mensagem.From = new MailAddress("daisymonte@gmail.com");
+            mensagem.To.Add("daisymonte@gmail.com");
+            mensagem.Subject = "Contato - Loja Virtual";
+            mensagem.Body = corpoMensagem;
+            mensagem.IsBodyHtml = true;
+
+            smtp.Send(mensagem);
+
+        }
+    }
+}
